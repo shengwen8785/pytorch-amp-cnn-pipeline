e2cd4da77ccdda13fd94458e651aaa83f914e74b
shengwen8785 <shengwen8785@gmail.com>
2025-07-03 23:52:06 +0800

    refactor(models): allow `get_model` to receive num_classes from config/
    - Adjusted `get_model` to accept `classes` setting from .yaml config file

--- models/create_models.py
+++ models/create_models.py
@@ -19,7 +19,7 @@ def initialize_models(local_rank:int, configs:dict):
     """
     display_message(logger, local_rank, f"Loading model architecture and initialing weights: {configs['model']}")
     with torch_distributed_zero_first(local_rank):
-        model = get_model(configs['model'], weights=configs['weights'])
+        model = get_model(configs['model'], weights=configs['weights'], num_classes=configs['classes'])
     display_message(logger, local_rank, f"Model architecture:/n{model}")
 
     return model
