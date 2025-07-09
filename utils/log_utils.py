bd599b6801ab87ea3cff8080fac49f790ec3738b
shengwen8785 <shengwen8785@gmail.com>
2025-07-09 15:20:56 +0800

    refactor(utils): allow get_logger to receive current_time
    - Modified `get_logger` to accept `current_time` for naming the log directory

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 _logger = None  # Define a global logger variable
 
-def get_logger(file_name="__main__", log_name="app.log", log_dir="./logs", log_level=logging.INFO, console_output=True, wandb_project=None, wandb_name=None):
+def get_logger(file_name="__main__", log_name="app.log", log_dir="./logs", log_level=logging.INFO, console_output=True, wandb_project=None, wandb_name=None, current_time=None):
     """
     Get the global logger (create if not already created).
 
@@ -16,6 +16,7 @@ def get_logger(file_name="__main__", log_name="app.log", log_dir="./logs", log_l
         console_output (bool): whether to display messages on standard output (console).
         wandb_project (str): W&B project name for dynamic log naming.
         wandb_name (str): W&B run name for dynamic log naming.
+        current_time (str): current time for dynamic log naming.
 
     Returns:
         logger (logging.Logger): initialized logger.
@@ -25,16 +26,21 @@ def get_logger(file_name="__main__", log_name="app.log", log_dir="./logs", log_l
         # Initialize logger only once
         root_path = str(Path(__file__).parent.parent)
 
+        # Define dynamic file name for logs
+        if wandb_project and wandb_name:
+            log_dir = f"{log_dir}/{wandb_project}/{wandb_name}"
+            dynamic_log_name = f"{wandb_project}_{wandb_name}.log"
+        else:
+            log_dir = log_dir
+            dynamic_log_name = log_name
+
+        if current_time is not None:
+            log_dir = f"{log_dir}/{current_time}"
+
         # Ensure 'log_dir' existed
         log_dir_path = os.path.join(root_path, log_dir)
         if not os.path.exists(log_dir_path):
             os.makedirs(log_dir_path, exist_ok=True)
-
-        # Define dynamic file name for logs
-        if wandb_project and wandb_name:
-            dynamic_log_name = f"{wandb_project}_{wandb_name}.log"
-        else:
-            dynamic_log_name = log_name
 
         # Form log file path
         log_file_path = os.path.join(log_dir_path, dynamic_log_name)
@@ -68,6 +74,8 @@ def get_logger(file_name="__main__", log_name="app.log", log_dir="./logs", log_l
 
 
 if __name__ == "__main__":
+    from datetime import datetime
+    current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
     print(__name__)
-    logger = get_logger(file_name=__name__)
+    logger = get_logger(file_name=__name__, wandb_project="test", wandb_name="test_log", current_time=current_time)
     logger.info("This is a test log.")
